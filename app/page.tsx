d79d6516f706665567584ac4d72d9f70bdad21d4
Artizmo <Brian@DESKTOP-PH30CK2>
2023-10-15 17:00:36 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+const prisma = new PrismaClient()
+
+export default async function Home() {
+  const accounts = await prisma.account.findMany()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +13,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <p>accounts length {accounts[0].email}</p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
