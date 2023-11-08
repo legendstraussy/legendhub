c43073546b92f9902b34050c13bebe33c389900b
legendStraussy <straussy@legendmud.org>
2023-11-08 17:12:49 -0600

    connects to mysql container

--- cron/test.js
+++ cron/test.js
@@ -1,3 +1,23 @@
-const mysql = require('mysql')
+import mysql from 'mysql2'
+import 'dotenv/config'
+
+const database = mysql.createConnection({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'hub',
+  port: process.env.MYSQL_PORT || 3306    
+})
+
+database.connect()
+
+const sqlQuery = 'SELECT * FROM accounts'
+
+database.query(sqlQuery, (err, result) => {
+  if (err) throw err;
+
+  console.log('ACCOUNTS: ', JSON.stringify(result))
+  database.destroy()
+})
 
 console.log('bingo from JavaScript!')
