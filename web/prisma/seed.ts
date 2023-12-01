7ff8ed61a3b755b0996a424de7fdf739b7dc8b94
legendStraussy <straussy@legendmud.org>
2023-11-30 22:41:40 -0600

    prisma fixes

--- web/prisma/seed.ts
+++ web/prisma/seed.ts
@@ -1,29 +1,29 @@
-import { PrismaClient } from './generated/client'
-// import { PrismaClient as OldHubClient } from './generated/client-oldhub'
+// import { PrismaClient } from './generated/client'
+// // import { PrismaClient as OldHubClient } from './generated/client-oldhub'
 
-const hub = new PrismaClient()
-// const oldhub = new OldHubClient()
+// const hub = new PrismaClient()
+// // const oldhub = new OldHubClient()
 
-async function main() {
-  // console.log(`Start seeding...`)
-  // const items = await oldhub.items.findMany()
-  // const item = items[1200]
-  // await hub.item.create({
-  //   data: {
-  //     shortDescription: 'Test Item',
-  //     createdBy: 1
-  //   }
-  // })
-}
+// async function main() {
+//   // console.log(`Start seeding...`)
+//   // const items = await oldhub.items.findMany()
+//   // const item = items[1200]
+//   // await hub.item.create({
+//   //   data: {
+//   //     shortDescription: 'Test Item',
+//   //     createdBy: 1
+//   //   }
+//   // })
+// }
 
-main()
-  .then(async () => {
-    await hub.$disconnect()
-    // await oldhub.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await hub.$disconnect()
-    // await oldhub.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await hub.$disconnect()
+//     // await oldhub.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await hub.$disconnect()
+//     // await oldhub.$disconnect()
+//     process.exit(1)
+//   })
